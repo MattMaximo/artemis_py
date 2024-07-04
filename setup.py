ea04d670638fd56378c7c7ceed2ff3d32e0bf37c
mattmaximo <mattmaximo@hotmail.com>
2024-07-03 21:44:08 -0400

    fix

--- setup.py
+++ setup.py
@@ -3,14 +3,20 @@ from setuptools import setup, find_packages
 setup(
     name="PyArtemis",
     version="0.1.0",
-    description="A package to interact with the Artemis API",
-    author="Matt Maximo",
-    author_email="matt@pioneerdigital.org",
+    description="A package to interact with the Artemis API. Blame ChatGPT for errors.",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/MattMaximo/PyArtemis",  
     packages=find_packages(),
     install_requires=[
         "pandas",
         "requests",
         "plotly"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
 )
