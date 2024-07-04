40328205f535e0de8ece95c3ee6c02c28244286c
mattmaximo <mattmaximo@hotmail.com>
2024-07-03 22:16:12 -0400

    fix

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="PyArtemis",
+    name="artemis_py",
     version="0.1.0",
     description="A package to interact with the Artemis API. Blame ChatGPT for errors.",
     author="Matt Maximo",
     author_email="matt@pioneerdigital.org",
-    url="https://github.com/MattMaximo/PyArtemis",  
+    url="https://github.com/MattMaximo/artemis_py",  
     packages=find_packages(),
     install_requires=[
         "pandas",
